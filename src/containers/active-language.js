8aa07f68db13d43c62add857fdcfb5f4fa839e6d
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-16 12:06:39 +0800

    connect reducer to render onclick action. set initail render using the first/index on data

--- src/containers/active-language.js
+++ src/containers/active-language.js
@@ -1,13 +1,42 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const ActiveLanguage = () => {
-  return (
-    <div className="col-md-8 text-center">
-      <h4>Javascript</h4>
-      <p><strong>Description :</strong> JavaScript ("JS" for short) is a full-fledged dynamic programming language that, when applied to an HTML document, can provide dynamic interactivity on websites. It was invented by Brendan Eich, co-founder of the Mozilla project, the Mozilla Foundation, and the Mozilla Corporation.</p>
-      <p><strong>Proficiency :</strong> 7 / 10</p>
-    </div>
-  )
+class ActiveLanguage extends React.Component {
+  initRender() {
+    return (
+      <div>
+        <h4>{this.props.myproglangs[0].name}</h4>
+        <p><strong>Description :</strong> {this.props.myproglangs[0].description}</p>
+        <p><strong>Proficiency :</strong> {this.props.myproglangs[0].proficiency} / 10</p>
+      </div>
+    )
+  }
+
+  selectRender() {
+    return (
+      <div>
+        <h4>{this.props.myproglang.name}</h4>
+        <p><strong>Description :</strong> {this.props.myproglang.description}</p>
+        <p><strong>Proficiency :</strong> {this.props.myproglang.proficiency} / 10</p>
+      </div>
+    )
+  }
+
+  render() {
+    const renderOption = !this.props.myproglang ? this.initRender() : this.selectRender()
+    return (
+      <div className="col-md-8 text-center">
+        {renderOption}
+      </div>
+    )
+  }
 }
 
-export default ActiveLanguage
+const mapStateToProps = (state) => {
+  return {
+    myproglangs: state.myproglangs,
+    myproglang: state.activeProglang
+  }
+}
+
+export default connect(mapStateToProps)(ActiveLanguage)
