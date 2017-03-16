f5c4c3511b481fb433c88d0e8faed2121d9cb42a
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-16 10:27:52 +0800

    import the 2 new component and render to view

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
+
+// COMPONENTS
+import LanguageList from './language_list'
+import ActiveLanguage from './active_language'
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div className="container-fluid">
+        <h3 className="page-header text-center">My Programming Languages</h3>
+        <div className="row">
+          <LanguageList />
+          <ActiveLanguage />
+        </div>
+      </div>
     );
   }
 }
