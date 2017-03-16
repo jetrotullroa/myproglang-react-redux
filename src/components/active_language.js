e1c8cd3fe806cc62d47fa5b25a2eb92fdeff761d
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-16 10:27:03 +0800

    create two static component

---
+++ src/components/active_language.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ActiveLanguage = () => {
+  return (
+    <div className="col-md-8 text-center">
+      <h4>Javascript</h4>
+      <p><strong>Description :</strong> JavaScript ("JS" for short) is a full-fledged dynamic programming language that, when applied to an HTML document, can provide dynamic interactivity on websites. It was invented by Brendan Eich, co-founder of the Mozilla project, the Mozilla Foundation, and the Mozilla Corporation.</p>
+      <p><strong>Proficiency :</strong> 7 / 10</p>
+    </div>
+  )
+}
+
+export default ActiveLanguage
---
+++ src/components/language_list.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const LanguageList = () => {
+  return (
+    <div className="col-md-4">
+      <ul className="list-group">
+        <li className="list-group-item">Javascript</li>
+        <li className="list-group-item">Elixir</li>
+        <li className="list-group-item">Elm</li>
+        <li className="list-group-item">Ruby</li>
+      </ul>
+    </div>
+  )
+}
+
+
+export default LanguageList
